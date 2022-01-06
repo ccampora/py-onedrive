932ba48fe16f0e94c90adcf294be26dc4c60696b
ccampora <ccampora@gmail.com>
2022-01-06 17:02:13 +0100

    move logging setup up to top

--- Onedrive.py
+++ Onedrive.py
@@ -1,15 +1,18 @@
 import logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 from Authentication import authenticate
 from Operations import sync_onedrive_to_disk
 from Config import create_pyonedrive_config_folder
 from Config import init_onedrive_database
-from Config import init_config
 from Globals import LOGGER as logger
+
 
 def start():
     create_pyonedrive_config_folder()
-    init_config()
+    init_onedrive_database()
     # Authentication
     logger.info("Authenticating...")
     authenticate()
@@ -19,10 +22,6 @@ def start():
     
     
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    
     logger = logging.getLogger();
     logger.info('Starting...')
     start()
